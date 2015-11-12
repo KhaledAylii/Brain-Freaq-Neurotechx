403996d3fd35305659eb8c915586ab56bb5b788c
Khaled Ali <al_khale@encs.concordia.ca>
2015-11-11 20:02:36 -0500

    Extracted lists of sensor info from callback functions.

--- OSC-Reciever.py
+++ OSC-Reciever.py
@@ -2,8 +2,9 @@ from liblo import *
 
 import sys 
 import time
-
-
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 class MuseServer(ServerThread):
     #listen for messages on port 5000
     def __init__(self):
@@ -13,21 +14,30 @@ class MuseServer(ServerThread):
     @make_method('/muse/elements/alpha_absolute', 'ffff')
     def alpha_callback(self, path, args):
         l_ear, l_forehead, r_forehead, r_ear = args
-        print "%s %f %f %f %f" % (path, l_ear, l_forehead, r_forehead, r_ear)
+        self.alpha = list(args)
+    @make_method('/muse/elements/alpha_absolute', 'ffff')
+    def delta_callback(self, path, args):
+        l_ear, l_forehead, r_forehead, r_ear = args
+        self.delta = list(args)
     @make_method('/muse/elements/beta_absolute', 'ffff')
     def beta_callback(self, path, args):
         l_ear, l_forehead, r_forehead, r_ear = args
-        print "%s %f %f %f %f" % (path, l_ear, l_forehead, r_forehead, r_ear)
+        self.beta = list(args)
     @make_method('/muse/elements/theta_absolute', 'ffff')
     def theta_callback(self, path, args):
         l_ear, l_forehead, r_forehead, r_ear = args
-        print "%s %f %f %f %f" % (path, l_ear, l_forehead, r_forehead, r_ear)
+        self.theta = list(args)
     @make_method('/muse/elements/gamma_absolute', 'ffff')
     def gamma_callback(self, path, args):
         l_ear, l_forehead, r_forehead, r_ear = args
-        gamma = list(args)
-        print gamma
-        print "%s %f %f %f %f" % (path, l_ear, l_forehead, r_forehead, r_ear)
+        self.gamma = list(args)
+    alpha = [0]
+    delta = [0]
+    beta = [0]
+    theta = [0]
+    gamma = [0]
+
+    
 try:
     server = MuseServer()
 except ServerError, err:
@@ -38,4 +48,6 @@ except ServerError, err:
 server.start()
 if __name__ == "__main__":
     while 1:
+        
         time.sleep(1)
+
